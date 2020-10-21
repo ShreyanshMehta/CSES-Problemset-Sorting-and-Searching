bf90584bf24164f5759c7b11996561935332324e
ShreyanshMehta <56556425+ShreyanshMehta@users.noreply.github.com>
2020-10-21 16:35:10 +0530

    Add files via upload

---
+++ Sum of Three Values.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define   fi(a,b) for(int i=a;i<b;i++)
+#define   fj(a,b) for(int j=a;j<b;j++)
+#define   ff first
+#define   ss second
+#define   ll long long
+#define   ld long double
+#define   ull unsigned long long
+#define   bp(x) __builtin_popcount(x)
+#define   pr(x) for(auto it: x) cout<<it<<" "; cout<<endl;
+#define   getMax(x) max_element(x.begin(),x.end())
+#define   getMin(x) min_element(x.begin(),x.end())
+#define   endl "\n"
+typedef   vector<int> vi;
+typedef   vector< pair<int,int> > vii;
+typedef   vector<long long> vl;
+typedef   pair<int,int> pii;
+typedef   pair<ll,ll> pll;
+typedef   vector< pair<ll,ll> > vll;
+//int dx[]={1,0,-1,0};
+//int dy[]={0,1,0,-1};
+//int dx[]={-1,0,1,1,1,0,-1,-1};
+//int dy[]={-1,-1,-1,0,1,1,1,0};
+
+void nikal_pehli_fursat_mai(){
+    ll n,x;
+    cin>>n>>x;
+
+    vector<array<int,2>>vk(n);
+    fi(0, n) {
+        cin>>vk[i][0];
+        vk[i][1] = i+1;
+    }
+
+    sort(vk.begin(), vk.end());
+
+    vi v;
+    fi(0, n){
+        v.push_back(vk[i][0]);
+    }
+
+    fi(0, n){
+        fj(i+1, n){
+            if(v[i] + v[j] > x) continue;
+            
+            auto it = lower_bound(v.begin()+j+1, v.end(), x - (v[i]+v[j]));
+
+            if(it!=v.end() and *it == x - (v[i] + v[j])){
+
+                cout<<vk[i][1]<<" "<<vk[j][1]<<" "<<vk[it-v.begin()][1]<<endl;
+                return;
+                
+            }
+        }
+    }
+    cout<<"IMPOSSIBLE"<<endl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tc=1; 
+    // cin>>tc;
+    while(tc--){
+    nikal_pehli_fursat_mai();
+    }
+}
