e51c7afc8fe34b5a180a2d384bd3d887bc79a3c8
ShreyanshMehta <56556425+ShreyanshMehta@users.noreply.github.com>
2020-10-21 18:30:10 +0530

    Add files via upload

---
+++ Nearest Smallest Value.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define   fi(a,b) for(int i=a;i<b;i++)
+#define   fj(a,b) for(int j=a;j<b;j++)
+#define   ff first
+#define   ss second
+#define   ll long long
+#define   ld long double
+#define   ull unsigned long long
+#define   bp(x) __builtin_popcount(x)
+#define   pr(x) for(auto it: x) cout<<it<<" "; cout<<endl;
+#define   getMax(x) max_element(x.begin(),x.end())
+#define   getMin(x) min_element(x.begin(),x.end())
+#define   endl "\n"
+typedef   vector<int> vi;
+typedef   vector< pair<int,int> > vii;
+typedef   vector<long long> vl;
+typedef   pair<int,int> pii;
+typedef   pair<ll,ll> pll;
+typedef   vector< pair<ll,ll> > vll;
+//int dx[]={1,0,-1,0};
+//int dy[]={0,1,0,-1};
+//int dx[]={-1,0,1,1,1,0,-1,-1};
+//int dy[]={-1,-1,-1,0,1,1,1,0};
+
+void nikal_pehli_fursat_mai(){
+    int n;
+    cin>>n;
+    
+    stack<pii>s;
+    cout<<0<<" ";
+
+    int temp;
+    cin>>temp;
+    s.push({temp, 1});
+
+    fi(2, n+1){
+        int temp;
+        cin>>temp;
+        while(!s.empty() && s.top().ff>=temp){
+            s.pop();
+        }
+
+        if(!s.empty()){
+            cout<<s.top().ss<<" ";
+        }
+        else{
+            cout<<0<<" ";
+        }
+
+        s.push({temp, i});
+    }
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tc=1; 
+    // cin>>tc;
+    while(tc--){
+    nikal_pehli_fursat_mai();
+    }
+}
