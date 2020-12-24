77a6c3ebc93ef1ef432b84bbdc9bb2e56497ae9a
ShreyanshMehta <56556425+ShreyanshMehta@users.noreply.github.com>
2020-12-24 19:04:23 +0530

    Update Sum of Four Values.cpp

--- Sum of Four Values.cpp
+++ Sum of Four Values.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 #define   fi(a,b) for(int i=a;i<b;i++)
 #define   fj(a,b) for(int j=a;j<b;j++)
 #define   ff first
@@ -23,41 +23,40 @@ typedef   vector< pair<ll,ll> > vll;
 //int dy[]={0,1,0,-1};
 //int dx[]={-1,0,1,1,1,0,-1,-1};
 //int dy[]={-1,-1,-1,0,1,1,1,0};
-
+ 
 void nikal_pehli_fursat_mai(){
     ll n,x;
     cin>>n>>x;
-
+ 
     vl v(n);
     fi(0, n) cin>>v[i];
-
+ 
     set<array<ll,3>>sum;
-
+ 
     fi(0, n){
         fj(i+1, n) sum.insert({v[i]+v[j], i+1, j+1});
     }
-
-    fi(0, n-2){
-        fj(0, i){
+ 
+    fi(0, n){
+        fj(i+1, n){
             ll temp = x - (v[i]+v[j]);
             
             array<ll,3>tup = {temp, -1, -1};
             auto it = sum.lower_bound(tup);
-
-            while(it!=sum.end() and (*it)[0]==temp and ((*it)[1]<=i+1 or (*it)[2]<=i+1)){
+ 
+            while(it!=sum.end() and (*it)[0]==temp and ((*it)[1]==i+1 or (*it)[2]==i+1 or (*it)[2]==j+1 or (*it)[1]==j+1)){
                 it = next(it);
             }
-
-            if((*it)[0]==temp) {
-                cout<<i+1<<" "<<j+1<<" "<<(*it)[1]<<" "<<(*it)[2]<<endl;
-                return;
-            }
+            
+            if(it==sum.end() or (*it)[0]!=temp) continue;
+            cout<<i+1<<" "<<j+1<<" "<<(*it)[1]<<" "<<(*it)[2]<<endl;
+            return;
         }
     }
-
+ 
     cout<<"IMPOSSIBLE"<<endl;
 }
-
+ 
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
